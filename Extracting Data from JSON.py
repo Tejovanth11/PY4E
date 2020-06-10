3eef5651c294b441f7f5a6c7bf5bead506342b16
Tejovanth <tejovanth84265@gmail.com>
2020-06-10 16:06:08 +0530

    Add files via upload

---
+++ Extracting Data from JSON.py
@@ -0,0 +1,14 @@
+import json
+
+data = '''
+[
+  *upload your data here*
+]'''
+
+info = json.loads(data)
+comments = len("comments")
+for item in range["comments:"]:
+	print (item["count"])
+	number = int(item["count"])
+	count = count + number
+print (count)
